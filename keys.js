9d9905ef9cbf4ef0fcf5b39143c2e02e4b4c4c08
John Silkey <jpsilkjr86@gmail.com>
2017-06-21 16:28:48 -0700

    added keys.js with twitter key info

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'P2DL1faHQ4ARZyzAQZb75do1z',
+  consumer_secret: 'N0Yyb6IgTD6SBZvwkswrltu33ffsf4ftfWgN3ZuKPzhhrggWDK',
+  access_token_key: '877663579079450624-Sz2Ajs9K5vSxklZqbOQG6lbFmbLD0Du',
+  access_token_secret: '3kKF9DDR702I5Jj8Ud1jSxGY2g9AtbVMDLsleFxUmhADA'
+};
