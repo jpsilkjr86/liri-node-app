01706a816e732521e6afc96935a731da289f6093
John Silkey <jpsilkjr86@gmail.com>
2017-06-21 18:20:01 -0700

    got node commands after index 2 to save as a single string instead of array for convenience in searching spotify and omdb

--- liri.js
+++ liri.js
@@ -1,13 +1,28 @@
-
-
 var userCmd = process.argv;
+
+var searchArray = [];
+var searchString = '';
 
 // const fs = require('fs');
 // var keys = {};
 var keys = require('./keys.js');
 
-if (userCmd[2] != null) {userCmd[2] = userCmd[2].toLowerCase();}
+// keeps the main command in lowercase for simplicity's sake
+if (userCmd[2] != null) {
+	userCmd[2] = userCmd[2].toLowerCase();
+}
 
+// joins arguments together with spaces if arguments exist after the main command.
+// for example if the user types "node liri.js spotify-this-song Hello Goodbye",
+// this will convert userCmd[3] and userCmd[4] into a single string "Hello Goodbye"
+if (userCmd.length > 3) {
+	for (let i = 3; i < userCmd.length; i ++) {
+		searchArray.push(userCmd[i]);
+	}
+	searchString = searchArray.join(' ');
+}
+
+// switch statement on the main command, at userCmd[2]
 switch (userCmd[2]) {
 	case 'my-tweets':
 		// call myTweets() function
@@ -15,11 +30,11 @@ switch (userCmd[2]) {
 		break;
 	case 'spotify-this-song':
 		// call spotifySong() function
-		spotifySong('Hello Goodbye');
+		spotifySong(searchString);
 		break;
 	case 'movie-this':
 		// call movieInfo() function
-		movieInfo('Some movie');
+		movieInfo(searchString);
 		break;
 	case 'do-what-it-says':
 		// call doWhatItSays() function
@@ -29,7 +44,7 @@ switch (userCmd[2]) {
 		// logs error message
 		console.log('Valid arguments are:\n* my-tweets\n* spotify-this-song <some song>\n'
 					+ '* movie-this <some movie>\n* do-what-it-says');
-}
+} // end of switch
 
 function myTweets() {
 	console.log('myTweets() function called.');
