b8c1b907a579dc6c781b01a1252a5ad7d2df3c3a
John Silkey <jpsilkjr86@gmail.com>
2017-06-23 00:13:19 -0700

    got my-tweets to display last two tweets onto console!

--- liri.js
+++ liri.js
@@ -1,11 +1,13 @@
+// Capture all user arguments in userCmd
 var userCmd = process.argv;
 
+// for use later in joining together any arguments from [3] and after
 var searchArray = [];
 var searchString = '';
 
-// const fs = require('fs');
-// var keys = {};
-var keys = require('./keys.js');
+const keys = require('./keys.js');
+
+var Twitter = require('twitter');
 
 // keeps the main command in lowercase for simplicity's sake
 if (userCmd[2] != null) {
@@ -48,6 +50,31 @@ switch (userCmd[2]) {
 
 function myTweets() {
 	console.log('myTweets() function called.');
+	var client = new Twitter ({
+		consumer_key: keys.twitterKeys.consumer_key,
+		consumer_secret: keys.twitterKeys.consumer_secret,
+		access_token_key: keys.twitterKeys.access_token_key,
+		access_token_secret: keys.twitterKeys.access_token_secret
+	});
+
+	var params = {
+		screen_name: 'nodejs'
+	};
+
+	client.get('statuses/user_timeline', function(error, tweets, response) {
+		if(error) {console.log(error);}
+		else {
+			console.log(tweets[0].text, '\n\n', tweets[1].text);  // text of tweets
+		}
+	  // console.log(response);  // Raw response object. 
+	});
+	// client.post('statuses/update', {status: "I'm using nodeJS to do this holy shit"})
+	// .then(function (tweet) {
+	// 	console.log(tweet);
+	// })
+	// .catch(function (error) {
+	// 	throw error;
+	// });
 }
 
 function spotifySong(song) {
