72c2ec314cd47a4897dff216092e2e2573d0f9ab
John Silkey <jpsilkjr86@gmail.com>
2017-06-23 12:13:00 -0700

    added keys.js in movie-this directory. got omdb server to respond to movie queries

---
+++ movie-this/keys.js
@@ -0,0 +1,5 @@
+console.log('this is loaded');
+
+exports.omdbKeys = {
+	apiKey: 'd20f646e'
+};
--- movie-this/movie-this.js
+++ movie-this/movie-this.js
@@ -1,6 +1,20 @@
+var request = require('request');
+var keys = require('./keys.js');
+
 function movieInfo(movie) {
-	console.log('movieInfo() function called.');
-	console.log('Chosen Movie: ' + movie);
+	var omdbApiKey = keys.omdbKeys.apiKey;
+
+	// query URL for OMDB API
+	var queryURL = 'https://www.omdbapi.com/?apikey=' + omdbApiKey 
+				+ '&t=' + movie;
+
+	request(queryURL, function (error, response, body) {
+		if(error) {
+			return console.log(error);
+		}
+		var result = JSON.parse(body);
+		console.log(result);
+	});
 }
 
 module.exports = movieInfo;
