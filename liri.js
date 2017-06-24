3dfc1895d298a22df39c6a1729971b490b891529
John Silkey <jpsilkjr86@gmail.com>
2017-06-24 00:13:59 -0700

    added input validation so that extra spaces wont mess up arguments and so that functions arent called if the extra argument is empty

--- liri.js
+++ liri.js
@@ -14,45 +14,82 @@ if (userCmd[2] != null) {
 // for example if the user types "node liri.js spotify-this-song Hello Goodbye",
 // this will convert userCmd[3] and userCmd[4] into a single string "Hello Goodbye"
 if (userCmd.length > 3) {
+	// push all non-empty userCmd's onto argArray
 	for (let i = 3; i < userCmd.length; i ++) {
-		argArray.push(userCmd[i]);
+		if (userCmd[i] != null && userCmd[i] !== '' && userCmd[i] !== ' ') {
+			argArray.push(userCmd[i]);
+		}
 	}
-	argString = argArray.join(' ');
+	// join all argArray elements together into a string with a space between each
+	if (argArray.length > 0) {
+		argString = argArray.join(' ');
+	}
 }
 
-// switch statement on the main command, at userCmd[2]
-switch (userCmd[2]) {
-	case 'my-tweets':
-		var tweet = require('./tweet/tweet.js');
-		// call myTweets() function
-		tweet.myTweets();
-		break;
-	case 'new-tweet':
-		var tweet = require('./tweet/tweet.js');
-		// call myTweets() function
-		tweet.newTweet(argString);
-		break;
-	case 'spotify-this-song':
-		var spotifySong = require('./spotify-this-song/spotify-this-song.js');
-		// call spotifySong() function
-		spotifySong(argString);
-		break;
-	case 'movie-this':
-		var movieInfo = require('./movie-this/movie-this.js');
-		// call movieInfo() function
-		movieInfo(argString);
-		break;
-	case 'do-what-it-says':
-		var doWhatItSays = require('./do-what-it-says/do-what-it-says.js');
-		// call doWhatItSays() function
-		doWhatItSays();
-		break;
-	default:
-		// default message
-		console.log('\nValid arguments are:'
-				+ '\n* my-tweets'
-				+ '\n* new-tweet <tweet text>'
-				+ '\n* spotify-this-song <some song>'
-				+ '\n* movie-this <some movie>'
-				+ '\n* do-what-it-says');
-} // end of switch
+// for any inputs with no extra argument after userCmd[2]
+if (argArray.length === 0) {
+	// switch statement on the main command (userCmd[2])
+	switch (userCmd[2]) {
+		case 'my-tweets':
+			var tweet = require('./tweet/tweet.js');
+			// call myTweets() function
+			tweet.myTweets();
+			break;
+		case 'do-what-it-says':
+			var doWhatItSays = require('./do-what-it-says/do-what-it-says.js');
+			// call doWhatItSays() function
+			doWhatItSays();
+			break;
+		default:
+			// default message
+			console.log('\nValid arguments are:'
+					+ '\n* my-tweets'
+					+ '\n* new-tweet <tweet text>'
+					+ '\n* spotify-this-song <some song>'
+					+ '\n* movie-this <some movie>'
+					+ '\n* do-what-it-says');
+	} // end of switch
+} 
+// for any inputs with an argument after the main command (userCmd[3] and after)
+else {
+	// switch statement on the main command (userCmd[2])
+	switch (userCmd[2]) {
+		// this case is repeated here in the event the user accidentally types an extra argument
+		// after userCmd[2]. this allows the program to run do-what-it-says either way.
+		case 'my-tweets':
+			var tweet = require('./tweet/tweet.js');
+			// call myTweets() function
+			tweet.myTweets();
+			break;
+		case 'new-tweet':
+			var tweet = require('./tweet/tweet.js');
+			// call myTweets() function
+			tweet.newTweet(argString);
+			break;
+		case 'spotify-this-song':
+			var spotifySong = require('./spotify-this-song/spotify-this-song.js');
+			// call spotifySong() function
+			spotifySong(argString);
+			break;
+		case 'movie-this':
+			var movieInfo = require('./movie-this/movie-this.js');
+			// call movieInfo() function
+			movieInfo(argString);
+			break;
+		// this case is repeated here in the event the user accidentally types an extra argument
+		// after userCmd[2]. this allows the program to run do-what-it-says either way.
+		case 'do-what-it-says':
+			var doWhatItSays = require('./do-what-it-says/do-what-it-says.js');
+			// call doWhatItSays() function
+			doWhatItSays();
+			break;
+		default:
+			// default message
+			console.log('\nValid arguments are:'
+					+ '\n* my-tweets'
+					+ '\n* new-tweet <tweet text>'
+					+ '\n* spotify-this-song <some song>'
+					+ '\n* movie-this <some movie>'
+					+ '\n* do-what-it-says');
+	} // end of switch
+}
