c194708d5a9e353fbaad2c4bb9b6ea45175315be
John Silkey <jpsilkjr86@gmail.com>
2017-06-24 01:48:33 -0700

    added functionality for doWhatItSays as a property method of inputSwitch

--- input-switch.js
+++ input-switch.js
@@ -9,9 +9,9 @@ var inputSwitch = {
 				tweet.myTweets();
 				break;
 			case 'do-what-it-says':
-				var doWhatItSays = require('./do-what-it-says/do-what-it-says.js');
+				// var doWhatItSays = require('./do-what-it-says/do-what-it-says.js');
 				// call doWhatItSays() function
-				doWhatItSays();
+				inputSwitch.doWhatItSays();
 				break;
 			default:
 				// default message
@@ -52,9 +52,9 @@ var inputSwitch = {
 			// this case is repeated here in the event the user accidentally types an extra argument
 			// after userCmd[2]. this allows the program to run do-what-it-says either way.
 			case 'do-what-it-says':
-				var doWhatItSays = require('./do-what-it-says/do-what-it-says.js');
+				// var doWhatItSays = require('./do-what-it-says/do-what-it-says.js');
 				// call doWhatItSays() function
-				doWhatItSays();
+				inputSwitch.doWhatItSays();
 				break;
 			default:
 				// default message
@@ -65,7 +65,25 @@ var inputSwitch = {
 						+ '\n* movie-this <some movie>'
 						+ '\n* do-what-it-says');
 		} // end of switch
-	} // end of withArg
-};
+	}, // end of withArg
+	doWhatItSays: function() {
+		// grabs fs module
+		var fs = require('fs');
+		// reads from random.txt
+		fs.readFile('./random.txt', 'utf8', function(err, data){
+			// if there's a comma, split it at the comma and save data as an array
+			if (data.search(',') !== -1) {
+				var contents = data.split(',');
+				// call inputSwitch again and send it contents[0] as mainCmd
+				// and contents[1] as argString
+				inputSwitch.withArg(contents[0], contents[1]);
+			}
+			else {
+				// if there's no comma, just send data to inputSwitch.noArg()
+				inputSwitch.noArg(data);
+			}			
+		}); // end of fs.readFile
+	} // end of doWhatItSays
+}; // end of inputSwitch object
 
 module.exports = inputSwitch;
